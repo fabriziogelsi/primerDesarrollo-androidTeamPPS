d970eb3417c436f4dabc2e25bbe65659f912bd5e
Fabrizio Gelsi <Fabrizio Gelsi>
2017-02-05 13:27:27 -0300

    Initial commit

---
+++ app/src/main/java/com/solodroid/yourradioappsinglestation/Config.java
@@ -0,0 +1,14 @@
+package com.solodroid.yourradioappsinglestation;
+
+public class Config {
+
+    //your radio stream url
+    public static final String RADIO_STREAM_URL = "http://78.47.158.23:80/";
+
+    //set true to enable AdMob ads and set false to disable AdMob ads
+    public static final boolean ENABLE_ADMOB_ADS = true;
+
+    //counter number to show interstitial ad when play button is clicked
+    public static final int INTERSTITIAL_COUNTER_NUMBER = 3;
+
+}
