7f80e2763ff20a85b1bd949d30a5383aed5b9d15
fabriziogelsi <fabrizio.gelsi@gmail.com>
2017-02-05 13:33:54 -0300

    Se desactivó la publicidad en la aplicación

--- app/src/main/java/com/solodroid/yourradioappsinglestation/Config.java
+++ app/src/main/java/com/solodroid/yourradioappsinglestation/Config.java
@@ -6,7 +6,7 @@ public class Config {
     public static final String RADIO_STREAM_URL = "http://78.47.158.23:80/";
 
     //set true to enable AdMob ads and set false to disable AdMob ads
-    public static final boolean ENABLE_ADMOB_ADS = true;
+    public static final boolean ENABLE_ADMOB_ADS = false;
 
     //counter number to show interstitial ad when play button is clicked
     public static final int INTERSTITIAL_COUNTER_NUMBER = 3;
