b0f512fdb3acd7fc02608f185d2a8e55049dc8c0
leandro.luis.cuvelo <lea.cuvelo@hotmail.com>
2017-02-07 21:18:39 -0300

    Fixed errors DEMO 1

--- app/src/main/java/com/solodroid/yourradioappsinglestation/Config.java
+++ app/src/main/java/com/solodroid/yourradioappsinglestation/Config.java
@@ -3,7 +3,7 @@ package com.solodroid.yourradioappsinglestation;
 public class Config {
 
     //your radio stream url
-    public static final String RADIO_STREAM_URL = "http://78.47.158.23:80/";
+    public static final String RADIO_STREAM_URL = "http://streaming01.shockmedia.com.ar:9360/";
 
     //set true to enable AdMob ads and set false to disable AdMob ads
     public static final boolean ENABLE_ADMOB_ADS = false;
